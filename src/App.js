48494affce9cee837a89faf524bb3f1bf35375b6
Iqbal Fasri <iqbalx64@gmail.com>
2019-06-20 16:54:38 +0700

    Refactor code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const API_KEY = "adb4320356ef8660531c9ebcb8b0269e";
+
+    const POPULAR_ENDPOIT = () => {
+      return `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=id&f_has_lyrics=1&apikey=${API_KEY}`;
+    };
+
+    axios(POPULAR_ENDPOIT(), { method: "GET" })
+      .then(chart => {
+        let { track_list } = chart.data.message.body;
+        console.log(track_list)
+      })
+      .catch(error => console.log(error));
+  });
+
   return (
     <div className="App">
       <header className="App-header">
